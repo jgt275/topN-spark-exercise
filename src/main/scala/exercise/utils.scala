78ccb6bac8e1dfd480bb70643ad7326184ac6329
JIjo Thomas <jijo.thomas@ibm.com>
2020-09-23 12:05:36 -0400

    code commit

--- src/main/scala/exercise/utils.scala
+++ src/main/scala/exercise/utils.scala
@@ -13,24 +13,24 @@ import java.io.{File, PrintWriter}
 import java.io.{FileNotFoundException, IOException}
 
 object utils {
-  
+
   val logger = Logger.getLogger("UtilsLogger")
-  
+
  /** Downloads gzipped text file from a URL and extracts if it is compressed.
   *
   * @param url URL path and filename.
   * @param session Existing spark session.
-  * 
+  *
   * @example val filepath=fileDownloader("http://path/file.gz")
   * @return String path value of downloaded file
   * @throws Exception upon error.
-  */ 
+  */
   def fileDownloader(url: String, localpath: String): String = {
-    
+
     // Sample url: server/path/filetoDownload.gz
     val filename = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')) //Extract filenmae without extension from url, Example: filetoDownload
     val extension = url.substring(url.lastIndexOf('.') + 1) //Extract file extension from url, Example: gz
-    
+
     val saveAs = localpath + "/" + filename + "." + extension
 
     //Create folder if folder does not exist
@@ -48,7 +48,7 @@ object utils {
       case fe: FileNotFoundException => println("Missing file exception")
       case ex: Exception => ex.printStackTrace()
     }
-         
+
     if (extension == "gz") {
       logger.info("Source file is of type GZip, Extracting contents")
       s"gzip -d -k -f $saveAs".!
@@ -58,26 +58,26 @@ object utils {
       return(saveAs)
       }
   }
-  
-  
+
+
  /** Computes the top N algorithm of a field grouped by a key.
   *
   * @param df Dataframe.
   * @param n User provided argument for top-N.
   * @param groupingKey Key field to aggregate by.
   * @param groupingVar Variable field to compute frequency on.
-  * 
+  *
   * @return Dataframe with key, frequency and top frequent values
   * @throws Exception upon error.
   */
   def topNbyKey(df: DataFrame, n: Int, groupingKey: String, groupingVar: String): DataFrame = {
-    
-      val groupedDF = df.na.drop
-                        .groupBy(col(groupingKey), col(groupingVar))
+
+      val groupedDF = df.groupBy(col(groupingKey), col(groupingVar))
                         .agg(count(col(groupingVar)).alias("frequency"))
-                        
+                        .na.drop
+
       val window = Window.partitionBy(groupingKey).orderBy(desc("frequency"))
-    
+
       return (groupedDF.withColumn("frequency_rank", row_number().over(window))
                        .where(col("frequency_rank")<= n)
                        .orderBy(col(groupingKey), col("frequency_rank")))
