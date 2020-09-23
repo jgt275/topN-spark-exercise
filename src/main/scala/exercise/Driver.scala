df8e718e4986b3f4fdd27e8b846a03137e02e2ed
JIjo Thomas <jijo.thomas@ibm.com>
2020-09-22 23:02:09 -0400

    fixed indent

--- src/main/scala/exercise/Driver.scala
+++ src/main/scala/exercise/Driver.scala
@@ -12,19 +12,19 @@ object Driver {
 
   def main(args: Array[String]) {
 
-		val spark = SparkSession.builder.appName("TopN").master("local[*]").getOrCreate()
+    val spark = SparkSession.builder.appName("TopN").master("local[*]").getOrCreate()
 
-		import spark.implicits._
+    import spark.implicits._
 
-		//Check if all arguments are available
-		if (args.length != 2) {
-		  logger.error("Requires exactly 2 input arguments - url (String), N (Int)")
-		  sys.exit(0)
-		}
+    //Check if all arguments are available
+    if (args.length != 2) {
+      logger.error("Requires exactly 2 input arguments - url (String), N (Int)")
+      sys.exit(0)
+    }
 
-		// Retrieve filepath of dataset from compressed download
-		val filepath = utils.fileDownloader(url=args(0), localpath="/scala_exercise/data")
-		println(s"Input data file: $filepath")
+    // Retrieve filepath of dataset from compressed download
+    val filepath = utils.fileDownloader(url=args(0), localpath="/scala_exercise/data")
+    println(s"Input data file: $filepath")
 
     logger.info("Reading input file")
     val df = spark.read
@@ -32,7 +32,7 @@ object Driver {
                   .csv(filepath)
 
     //Set shuffle partitions based on input data
-		spark.conf.set("spark.sql.shuffle.partitions", 2*df.rdd.getNumPartitions)
+    spark.conf.set("spark.sql.shuffle.partitions", 2*df.rdd.getNumPartitions)
 
     val df_2 = df.select($"_c0".as("visitor")
                           ,to_date(ltrim(element_at(split($"_c3",":"),1),"["), "dd/MMM/yyyy").as("date") //Keep only date
@@ -40,16 +40,16 @@ object Driver {
                           ,$"_c6".as("response")
                           )
 
-		println(s"-- Displaying top-${args(1)} frequent visitor by day ranked from most to least and their frequency --")
-		val freq_visitor = utils.topNbyKey(df=df_2, n=args(1).toInt, groupingKey="date", groupingVar="visitor")
-		freq_visitor.show(false)
+    println(s"-- Displaying top-${args(1)} frequent visitor by day ranked from most to least and their frequency --")
+    val freq_visitor = utils.topNbyKey(df=df_2, n=args(1).toInt, groupingKey="date", groupingVar="visitor")
+    freq_visitor.show(false)
 
-		println(s"-- Displaying top-${args(1)} frequent url by day ranked from most to least and their frequency --")
-		var freq_url = utils.topNbyKey(df=df_2, n=args(1).toInt, groupingKey="date", groupingVar="url")
-		freq_url.show(false)
+    println(s"-- Displaying top-${args(1)} frequent url by day ranked from most to least and their frequency --")
+    var freq_url = utils.topNbyKey(df=df_2, n=args(1).toInt, groupingKey="date", groupingVar="url")
+    freq_url.show(false)
 
-		logger.info("Closing spark session")
-		spark.stop()
-	}
+    logger.info("Closing spark session")
+    spark.stop()
+  }
 
 }
